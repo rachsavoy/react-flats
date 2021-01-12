7de4247ec62ae74e4a571046be49529c316a16b2
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-01-12 17:18:50 +0900

    ARCH: made connections between files.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import SimpleMap from 'simplemap.jsx';
+import FlatList from 'flatlist.jsx';
+import SimpleMap from 'simplemap.jsx'
 
 class App extends Component {
   render() {
