87eaf317e4dca87448df334b3e872cb81eed3183
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-01-12 17:14:04 +0900

    ARCH: created flat list file.

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import SimpleMap from 'simplemap.jsx';
+
+class App extends Component {
+  render() {
+    return {
+
+    }
+  }
+}
---
+++ src/components/flatlist.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class FlatList extends Component {
+  render() {
+    return(
+    )
+  }
+}
+
+
