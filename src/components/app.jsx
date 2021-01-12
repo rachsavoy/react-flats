d298d72600948f695f8c345805a068eb0cf1444d
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-01-12 18:13:22 +0900

    FEAT: added list of flats.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import SimpleMap from 'simplemap.jsx';
+import GoogleMapReact from 'google-map-react';
+
+import flats from '../../data/flats';
 import FlatList from 'flatlist.jsx';
 import SimpleMap from 'simplemap.jsx'
+import Marker from './marker';
 
 class App extends Component {
-  render() {
-    return {
-
+  constructor(props) {
+    super(props) {
+      this.state = {
+        selectedFlat: flats[0],
+        flats
+      };
     }
+
+    center() {
+      return {
+        lat: this.state.selectedFlat.lat,
+        lng: this.state.selectedFlat.lng
+      };
+    }
+
+    selectFlat = (index) => {
+      this.setState({ selectedFlat: flats[index] });
+    }
+
+  render() {
+    return (
+      <div>
+        <Flatlist
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
   }
 }
+
+export default App;
