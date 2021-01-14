5b2b39dd02eecdc1312ea6f8bc612cef448f19b3
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-01-14 12:03:01 +0900

    FIX: indentings and deleting file.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,29 +2,28 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 import flats from '../../data/flats';
-import FlatList from 'flatlist.jsx';
-import SimpleMap from 'simplemap.jsx'
+import FlatList from './flat_list';
 import Marker from './marker';
 
 class App extends Component {
   constructor(props) {
-    super(props) {
-      this.state = {
-        selectedFlat: flats[0],
-        flats
-      };
-    }
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    };
+  }
 
-    center() {
-      return {
-        lat: this.state.selectedFlat.lat,
-        lng: this.state.selectedFlat.lng
-      };
-    }
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
 
-    selectFlat = (index) => {
-      this.setState({ selectedFlat: flats[index] });
-    }
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
 
   render() {
     return (
--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -9,6 +9,13 @@ class Flat extends Component {
     return(
       <div className={`card${this.props.selected ? 'active' : ''}`} style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), regba(0,0,0,0.2)), url('${this.props.flat.imageUrl}')` }}>
         <div className="card-category">{this.props.flat.price} {this.props.flat.priceCurrency}</div>
-    )
+        <div className="card-description">
+          <h2>{this.props.flat.name}</h2>
+        </div>
+        <a className="card-link" href="#" onclick={this.handleClick}></a>
+      </div>
+    );
   }
 }
+
+export default Flat;
